b788dcbc67f0dae751165431c6b00fb4c5359e2d
bal-19 <iqbalhaidee19@gmail.com>
2024-09-26 11:17:01 +0700

    add cache feature and fix null response

--- app/controller/library_controller.py
+++ app/controller/library_controller.py
@@ -1,14 +1,45 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Query
 from fastapi_cache.decorator import cache
 
-from ..service.crawler_service import LibraryCrawlerService
+from service.crawler_service import LibraryCrawlerService
 
 router = APIRouter()
 
-@router.get("/")
+@router.get("/perpustakaan")
 @cache(expire=3600)  # Cache this endpoint for 1 hour
-def get_libraries():
+def get_libraries(
+    jenis: str = Query('', description="Jenis perpustakaan (opsional)"),
+    provinsi_id: str = Query('', description="ID provinsi (opsional)"),
+    kabkota_id: str = Query('', description="ID kabupaten/kota (opsional)"),
+    kecamatan_id: str = Query('', description="ID kecamatan (opsional)"),
+    kelurahan_id: str = Query('', description="ID kelurahan (opsional)"),
+    length: int = Query(10, description="Jumlah data yang ingin diambil (opsional)")
+):
     # Instantiate the crawler service and fetch libraries data
-    service = LibraryCrawlerService()
-    libraries = service.fetch_libraries_data()
-    return libraries
+    try:
+        service = LibraryCrawlerService()
+        libraries = service.fetch_libraries_data(
+            jenis=jenis,
+            provinsi_id=provinsi_id,
+            kabkota_id=kabkota_id,
+            kecamatan_id=kecamatan_id,
+            kelurahan_id=kelurahan_id,
+            length=length
+        )
+
+        if libraries:
+            return {
+                'status': 'success',
+                **libraries
+            }
+            
+        else:
+            return {
+                'status': 'error',
+                **libraries
+            }
+
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error {e}")
+    
