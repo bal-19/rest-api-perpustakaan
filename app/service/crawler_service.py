6d1fcd634af5f92f274a710103c884272f524eb2
bal-19 <iqbalhaidee19@gmail.com>
2024-09-26 11:19:12 +0700

    fix null response

--- app/service/crawler_service.py
+++ app/service/crawler_service.py
@@ -1,11 +1,28 @@
-from ..helper.scraper import WebScraper
+from fastapi import HTTPException
+from typing import List
+
+from helper.scraper import WebScraper
 
 class LibraryCrawlerService:
     def __init__(self):
         self.scraper = WebScraper()
 
-    def fetch_libraries_data(self):
+    def fetch_libraries_data(self, jenis: str, provinsi_id: str, kabkota_id: str, kecamatan_id: str, kelurahan_id: str, length: int) -> List[dict]:
         url = "https://data.perpusnas.go.id/public/direktori/list"
-        # fetch data using scraper helper
-        libraries = self.scraper.scrape_libraries(url)
-        return libraries
+        
+        try:
+            # fetch data using scraper helper
+            libraries = self.scraper.scrape_libraries(
+                url,
+                jenis=jenis,
+                provinsi_id=provinsi_id,
+                kabkota_id=kabkota_id,
+                kecamatan_id=kecamatan_id,
+                kelurahan_id=kelurahan_id,
+                length=length
+            )
+            return libraries
+        
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Internal server error {e}")
+        
