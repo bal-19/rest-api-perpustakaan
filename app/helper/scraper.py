6308b5a129be716ee83eebd7018f4a9cdaf870fd
bal-19 <iqbalhaidee19@gmail.com>
2024-09-27 18:19:01 +0700

    add model and fix some bugs

--- app/helper/scraper.py
+++ app/helper/scraper.py
@@ -4,6 +4,7 @@ from fastapi import HTTPException
 from datetime import datetime
 from typing import List
 
+from model.perpustakaan import PerpustakaanMeta
 
 class WebScraper:
     def __init__(self) -> None:
@@ -32,26 +33,26 @@ class WebScraper:
             'x-requested-with': 'XMLHttpRequest',
         }
     
-    def mapping(self, data: dict) -> dict:
+    def mapping(self, data: dict) -> PerpustakaanMeta:
         # mapping
-        npp = data.get("npp")
-        nama = data.get("nama")
-        lembaga_induk = data.get("lembaga_induk")
-        alamat = data.get("alamat")
-        telepon = data.get("telepon")
-        email = data.get("email")
-        website = data.get("website")
-        jenis = data.get("jenis")
-        sub_jenis = data.get("subjenis")
-        status_perpus = data.get("status_perpus")
-        kode_pos = data.get("kode_pos")
-        provinsi = data.get("nama_provinsi")
-        kabkota = data.get("nama_kabkota")
-        kecamatan = data.get('nama_kecamatan')
-        kelurahan = data.get("nama_kelurahan")
+        npp = data.get("npp") if data.get("npp") else "-"
+        nama = data.get("nama") if data.get("nama") else "-"
+        lembaga_induk = data.get("lembaga_induk") if data.get("lembaga_induk") else "-"
+        alamat = data.get("alamat") if data.get("alamat") else "-"
+        telepon = data.get("telepon") if data.get("telepon") else "-"
+        email = data.get("email") if data.get("email") else "-"
+        website = data.get("website") if data.get("website") else "-"
+        jenis = data.get("jenis") if data.get("jenis").capitalize() else "-"
+        sub_jenis = data.get("subjenis") if data.get("subjenis") else "-"
+        status_perpus = data.get("status_perpus").capitalize() if data.get("status_perpus") else "-"
+        kode_pos = data.get("kode_pos") if data.get("kode_pos") else "-"
+        provinsi = data.get("nama_provinsi") if data.get("nama_provinsi") else "-"
+        kabkota = data.get("nama_kabkota") if data.get("nama_kabkota") else "-"
+        kecamatan = data.get('nama_kecamatan') if data.get("nama_kecamatan") else "-"
+        kelurahan = data.get("nama_kelurahan").capitalize() if data.get("nama_kelurahan") else "-"
 
         # saving to variable
-        raw_data = dict(
+        raw_data = PerpustakaanMeta(
             npp=npp,
             nama=nama,
             lembaga=lembaga_induk,
@@ -149,6 +150,6 @@ class WebScraper:
         # scraping logic
         for library in result.get("data"):
             data = self.mapping(library)
-            library_data.get("data").append(data)
+            library_data.get("data").append(data.__dict__)
 
         return library_data
