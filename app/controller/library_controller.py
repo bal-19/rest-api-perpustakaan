3180836bd633b04cda5e387e8770227460b497c5
bal-19 <iqbalhaidee19@gmail.com>
2024-09-28 07:49:29 +0700

    add new route

--- app/controller/library_controller.py
+++ app/controller/library_controller.py
@@ -4,63 +4,77 @@ from fastapi_cache.decorator import cache
 from service.crawler_service import LibraryCrawlerService
 
 router = APIRouter()
+service = LibraryCrawlerService()
 
-@router.get("/data")
+@router.get("/data", tags=["Perpustakaan"])
 @cache(expire=1800)  # Cache this endpoint for 30 minute
 async def get_libraries(
-    jenis: str = Query('', description="Jenis perpustakaan (opsional)"),
-    provinsi_id: str = Query('', description="ID provinsi (opsional)"),
-    kabkota_id: str = Query('', description="ID kabupaten/kota (opsional)"),
-    kecamatan_id: str = Query('', description="ID kecamatan (opsional)"),
-    kelurahan_id: str = Query('', description="ID kelurahan (opsional)"),
+    type_name: str = Query('', description="Jenis perpustakaan (opsional)"),
+    province_id: str = Query('', description="ID provinsi (opsional)"),
+    city_id: str = Query('', description="ID kabupaten/kota (opsional)"),
+    district_id: str = Query('', description="ID kecamatan (opsional)"),
+    subdistrict_id: str = Query('', description="ID kelurahan (opsional)"),
     length: int = Query(10, description="Jumlah data yang ingin diambil (opsional)")
 ):
     # Instantiate the crawler service and fetch libraries data
-    try:
-        service = LibraryCrawlerService()
-        libraries = service.fetch_libraries_data(
-            jenis=jenis,
-            provinsi_id=provinsi_id,
-            kabkota_id=kabkota_id,
-            kecamatan_id=kecamatan_id,
-            kelurahan_id=kelurahan_id,
-            length=length
-        )
+    libraries = service.fetch_libraries_data(
+        jenis=type_name,
+        provinsi_id=province_id,
+        kabkota_id=city_id,
+        kecamatan_id=district_id,
+        kelurahan_id=subdistrict_id,
+        length=length
+    )
 
-        if libraries:
-            return {
-                'status': 'success',
-                **libraries
-            }
-            
-        else:
-            return {
-                'status': 'error',
-                **libraries
-            }
+    if libraries.get("data"):
+        return {
+            'status': 'success',
+            **libraries
+        }
+        
+    else:
+        return {
+            'status': 'error',
+            'message': 'empty data',
+            **libraries
+        }
 
-    
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal server error {e}")
-
-@router.get("/jenis")
+@router.get("/type", tags=["Type"])
 @cache(expire=900) # cache for 15 minute
 async def get_type():
-    try:
-        service = LibraryCrawlerService()
-        types = service.fetch_type_data()
-        
-        if types:
-            return {
-                'status': 'success',
-                'types': types
-            }
+    types = service.fetch_type_data()
+    
+    if types:
+        return {
+            'status': 'success',
+            'types': types
+        }
+    
+@router.get("/subtype/{nama_jenis}", tags=["Type"])
+@cache(expire=900) # cache for 15 minute
+async def get_subtype(nama_jenis: str):
+    subtypes = service.fetch_subtype_data(nama_jenis)
+    
+    if subtypes:
+        return {
+            'status': 'success',
+            'subtypes': subtypes
+        }
 
-        else:
-            return {
-                'status': 'success',
-                'types': []
-            }
+@router.get("/region/province", tags=["Region"])
+@cache(expire=900) # cache for 15 minute
+async def get_province():
+    provinces = service.fetch_province_data()
+    
+    if provinces:
+        return {
+            'status': 'success',
+            'data': provinces
+        }
         
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal server error {e}")
+    else:
+        return {
+            'status': 'error',
+            'message': 'empty data',
+            'data': provinces
+        }
