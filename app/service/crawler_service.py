8038719c475529840b7d19fe6f015fe96ef98fe4
bal-19 <iqbalhaidee19@gmail.com>
2024-09-28 07:50:17 +0700

    add new service

--- app/service/crawler_service.py
+++ app/service/crawler_service.py
@@ -1,3 +1,5 @@
+import urllib.parse
+
 from fastapi import HTTPException
 from typing import List
 
@@ -7,32 +9,43 @@ class LibraryCrawlerService:
     def __init__(self):
         self.scraper = WebScraper()
 
+    # DATA PERPUSTAKAAN
     def fetch_libraries_data(self, jenis: str, provinsi_id: str, kabkota_id: str, kecamatan_id: str, kelurahan_id: str, length: int) -> List[dict]:
         url = "https://data.perpusnas.go.id/public/direktori/list"
         
-        try:
-            # fetch data using scraper helper
-            libraries = self.scraper.scrape_libraries(
-                url,
-                jenis=jenis,
-                provinsi_id=provinsi_id,
-                kabkota_id=kabkota_id,
-                kecamatan_id=kecamatan_id,
-                kelurahan_id=kelurahan_id,
-                length=length
-            )
-            return libraries
+        # fetch data using scraper helper
+        libraries = self.scraper.scrape_libraries(
+            url,
+            jenis=jenis,
+            provinsi_id=provinsi_id,
+            kabkota_id=kabkota_id,
+            kecamatan_id=kecamatan_id,
+            kelurahan_id=kelurahan_id,
+            length=length
+        )
+        return libraries
         
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Internal server error {e}")
-        
+    # TYPE
     def fetch_type_data(self) -> List:
         url = "https://data.perpusnas.go.id/reference/list-dropdown/jenis-perpustakaan"
         
-        try:
-            # fetch data
-            types = self.scraper.scrape_type(url)
-            return types
-            
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Internal server error {e}")
+        # fetch data
+        types = self.scraper.scrape_type(url)
+        return types
+    
+    # SUBTYPE
+    def fetch_subtype_data(self, type: str) -> List:
+        type_encode = urllib.parse.quote(type.upper())
+        url = f"https://data.perpusnas.go.id/reference/list-dropdown/subjenis-perpustakaan/{type_encode}"
+        
+        # fetch all subtype from type
+        subtypes = self.scraper.scrape_subtype(url)
+        return subtypes
+
+    # PROVINCE
+    def fetch_province_data(self) -> List[dict]:
+        url = "https://data.perpusnas.go.id/public/kewilayahan/dati1/list-dropdown"
+        
+        # fetch all province at Indonesia
+        provinces = self.scraper.scrape_province(url)
+        return provinces
