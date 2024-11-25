eaf6c5cd876059ac8b36a650f1233484ff4d5f17
bal-19 <iqbalhaidee19@gmail.com>
2024-11-25 12:16:16 +0700

    refactor code

--- source/service/crawler_service.py
+++ source/service/crawler_service.py
@@ -9,9 +9,10 @@ from ..helper.fetch import FetchDataPerpustakaan
 class LibraryCrawlerService:
     def __init__(self):
         self.scraper = WebScraper()
+        self.source = "https://data.perpusnas.go.id"
 
     def fetch_type_data(self) -> List:
-        url = "https://data.perpusnas.go.id/reference/list-dropdown/jenis-perpustakaan"
+        url = f"{self.source}/reference/list-dropdown/jenis-perpustakaan"
         
         # fetch data
         types = self.scraper.scrape_type(url)
@@ -20,7 +21,7 @@ class LibraryCrawlerService:
     # SUBTYPE
     def fetch_subtype_data(self, type: str) -> List:
         type_encode = urllib.parse.quote(type.upper())
-        url = f"https://data.perpusnas.go.id/reference/list-dropdown/subjenis-perpustakaan/{type_encode}"
+        url = f"{self.source}/reference/list-dropdown/subjenis-perpustakaan/{type_encode}"
         
         # fetch all subtype from type
         subtypes = self.scraper.scrape_type(url)
@@ -28,7 +29,7 @@ class LibraryCrawlerService:
 
     # PROVINCE
     def fetch_province_data(self) -> List[dict]:
-        url = "https://data.perpusnas.go.id/public/kewilayahan/dati1/list-dropdown"
+        url = f"{self.source}/public/kewilayahan/dati1/list-dropdown"
         
         # fetch all province at Indonesia
         provinces = self.scraper.scrape_region(url)
@@ -36,7 +37,7 @@ class LibraryCrawlerService:
 
     # CITY
     def fetch_city_data(self, id_provinsi: str) -> List[dict]:
-        url = f"https://data.perpusnas.go.id/public/kewilayahan/dati2/list-dropdown/{id_provinsi}"
+        url = f"{self.source}/public/kewilayahan/dati2/list-dropdown/{id_provinsi}"
         
         # fetch all city in province
         city = self.scraper.scrape_region(url)
@@ -44,7 +45,7 @@ class LibraryCrawlerService:
     
     # DISTRICT
     def fetch_district_data(self, id_kabkota: str) -> List[dict]:
-        url = f"https://data.perpusnas.go.id/public/kewilayahan/dati3/list-dropdown/{id_kabkota}"
+        url = f"{self.source}/public/kewilayahan/dati3/list-dropdown/{id_kabkota}"
         
         # fetch all district/kecamatan
         district = self.scraper.scrape_region(url)
@@ -52,7 +53,7 @@ class LibraryCrawlerService:
 
     # SUBDISTRICT
     def fetch_subdistrict_data(self, id_kecamatan: str) -> List[dict]:
-        url = f"https://data.perpusnas.go.id/public/kewilayahan/dati4/list-dropdown/{id_kecamatan}"
+        url = f"{self.source}/public/kewilayahan/dati4/list-dropdown/{id_kecamatan}"
         
         # fetch all subdistrict/kelurahan
         subdistrict = self.scraper.scrape_region(url)
