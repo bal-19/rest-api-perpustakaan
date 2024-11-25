3e8a44b4376adeb9c0d959dd9f3d77a9f9182ac4
bal-19 <iqbalhaidee19@gmail.com>
2024-11-25 16:47:28 +0700

    upgrade search endpoint

--- api/v1/endpoints/perpusnas.py
+++ api/v1/endpoints/perpusnas.py
@@ -7,41 +7,43 @@ from services.perpusnas_service import PerpusnasService
 
 router = APIRouter()
 
+# Library
 @router.get("/data", response_model=PerpusnasResponse, tags=["Perpustakaan"], description="Mengambil semua data perpustakaan tanpa filter")
 @cache(expire=1800)
 async def get_libraries(
-    service: PerpusnasService = Depends(),
     limit: int = Query(10, ge=5, le=50, description="Maximum amount of data to be displayed"),
-    page: int = Query(1, ge=1, description="The data page you want to retrieve")
+    page: int = Query(1, ge=1, description="The data page you want to retrieve"),
+    type: str = Query(None, description="The type of library you want to display"),
+    subtype: str = Query(None, description="The library subtype to display"),
+    province: str = Query(None, description="Province of the library you wish to display"),
+    city: str = Query(None, description="Regency/City library you wish to display"),
+    subdistrict: str = Query(None, description="Library Subdistrict you want to display"),
+    village: str = Query(None, description="The Village library you wish to display"),
+    service: PerpusnasService = Depends()
 ):
-    return await service.get_all(page=page, limit=limit)
+    filter_kwargs = {
+        "jenis": type,
+        "subjenis": subtype,
+        "provinsi": province,
+        "kabkota": city,
+        "kecamatan": subdistrict,
+        "kelurahan": village
+    }
+    
+    return await service.get_all(page=page, limit=limit, **filter_kwargs)
 
 @router.get("/data/search", response_model=PerpusnasResponse, tags=["Perpustakaan"], description="Mengambil semua data perpustakaan berdasarkan nama perpustakaan yang dicari")
 @cache(expire=1800)
 async def get_libraries_by_search(
     query: str,
-    service: PerpusnasService = Depends(),
     limit: int = Query(10, ge=5, le=50, description="Maximum amount of data to be displayed"),
-    page: int = Query(1, ge=1, description="The data page you want to retrieve")
+    page: int = Query(1, ge=1, description="The data page you want to retrieve"),
+    service: PerpusnasService = Depends()
 ):
     return await service.get_by_search(query, page=page, limit=limit)
 
-@router.get("/data/filter", response_model=PerpusnasResponse, tags=["Perpustakaan"], description="Mengambil semua data perpustakaan berdasarkan filter yang telah ditetapkan")
-@cache(expire=1800)
-async def get_libraries_by_filter(
-    key: str,
-    value: str,
-    service: PerpusnasService = Depends(),
-    limit: int = Query(10, ge=5, le=50, description="Maximum amount of data to be displayed"),
-    page: int = Query(1, ge=1, description="The data page you want to retrieve")
-):
-    """
-    Key for column name <br>
-    Value for value at column
-    """
-    return await service.get_by_filter(key, value, page=page, limit=limit)
 
-
+# Type & Subtype
 @router.get("/list/type", response_model=list, tags=["Type"], description="Mengambil jenis jenis perpustakaan yang tersedia")
 @cache(expire=3600)
 async def get_type(
@@ -58,6 +60,7 @@ async def get_subtype(
     return service.get_subtype(type_name)
 
 
+# Locations
 @router.get("/list/region/provinces", tags=["Region"], description="Mengambil semua Provinsi yang ada di Indonesia")
 @cache(expire=3600) 
 async def get_provinces(
